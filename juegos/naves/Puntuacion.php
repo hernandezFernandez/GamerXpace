122e68302045bc831a5c05f638f9a891dd6028ed
hernandezFernandez <81775411+hernandezFernandez@users.noreply.github.com>
2021-06-17 09:45:24 +0200

    Add files via upload

--- juegos/naves/Puntuacion.php
+++ juegos/naves/Puntuacion.php
@@ -1,9 +1,9 @@
 <?php
-
+// incluye las conexiones a la BBDD
 include '../../conexion.php';
-
+// decodifica los datos pasados del js
 $datos = json_decode(stripslashes($_POST['datos']));
-// no funciona la consulta por algun motivo
+// recoge de la BBDD la puntuacion total y maxima del usuario
 $MyBBDD->consulta("SELECT * FROM usuarios where id_usu = '" . $datos->user . "'");
     while ($fila = $MyBBDD->extraer_registro()) {
 
@@ -12,11 +12,11 @@ $MyBBDD->consulta("SELECT * FROM usuarios where id_usu = '" . $datos->user . "'"
       
     }
 
-
+// suma la puntuacion que tenia con la nueva
     $punt_actu = $punt_actu + $datos->punt;
-
+// actualiza la tabla de la BBDD con la nueva puntuacion total
 $MyBBDD->consulta("UPDATE usuarios SET totalPuntnav= '$punt_actu'  WHERE id_usu = '" . $datos->user . "'");
-
+// si la nueva puntuacion maxima es supoerior a la anterior la actualiza en la BBDD
 if( $puntosMax < $datos->punt){
   $MyBBDD->consulta("UPDATE usuarios SET maxPuntnav= '" . $datos->punt  . "'  WHERE id_usu = '" . $datos->user . "'");
 
--- juegos/naves/naves.js
+++ juegos/naves/naves.js
@@ -1,49 +1,44 @@
 
-var boton = document.getElementById("play");
+// creacion de variables
 var inicio = 0;
 
+var boton = document.getElementById("play");
 var iz = document.getElementById("iz");
 var der = document.getElementById("der");
 
 
-var scoreTotal = 0;
+var puntuacionTotal = 0;
 var nivel = 1;
-var CanteneNiv = 3
-var gameFin = null
+var CantEneNiv = 3
+var juegoFin = null
 
-function arkanoid(){
+function naves(){
+    // inicia el juego
     inicio = 1;
     
 
-
+// recoge el canvas en una variable y lo muestra
 var canvas = document.getElementById("myCanvas");
 canvas.style.display="block";
-
+// setea el ancho y el alto del canvas
 var ancho = main.clientWidth * 0.8;
 var alto = ((main.clientWidth * 0.8) /1.5);
-
 canvas.setAttribute('width', ancho); 
 canvas.setAttribute('height', alto); 
 
-
-
 var ctx = canvas.getContext("2d");
 
-
-
-console.log(nivel)
-
-// alto y ancho de la pala
+// alto y ancho de la nave
 var naveAncho = ancho * 0.15;
 var naveAlto = alto * 0.15;
 var naveY = canvas.height * 0.83;
 var naveX = canvas.width/2;
 
 // comprobar que boton esta pulsado
-var rightPressed = false;
-var leftPressed = false;
+var derechaP = false;
+var izquierdaP = false;
 // puntuacion
-var score = 0;
+var puntuacion = 0;
 // intervalo
 var id;
 // misiles
@@ -55,73 +50,77 @@ gameOver.src = "../../img/gameOver.png";
 var youWin = new Image();
 youWin.src = "../../img/youWin.jpg";
 
-
-var naveImage = new Image(); // nave
+// iamgen de la nave el fondo y los enemigos
+var naveImage = new Image(); 
 naveImage.src = "images/spaceship-pic.png";
 
-var enemiespic1  = new Image(); // enemigo 1
-enemiespic1.src     = "images/enemigo1.png";
+var eneFoto1  = new Image(); 
+eneFoto1.src     = "images/enemigo1.png";
     
-var enemiespic2 = new Image(); // enemigo 2
-enemiespic2.src     = "images/enemigo2.png"; //Enemies picture
+var eneFoto2 = new Image(); 
+eneFoto2.src     = "images/enemigo2.png"; 
 
 var backgroundImage = new Image();
 backgroundImage.src = "images/background-pic.jpg";
 
-
+// obtengo los botones de la version movil
 var iz = document.getElementById("iz");
 var der = document.getElementById("der");
+var fire = document.getElementById("fire");
 
+// recoge las pulsaciones de las teclas y los botones
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
-
-// document.addEventListener("mousemove", mouseMoveHandler, false);
 
 iz.addEventListener("touchstart",function(e){ keyDownHandler(e, "iz")} , false);
 der.addEventListener("touchstart",function(e){ keyDownHandler(e, "der")}, false);
 iz.addEventListener("touchend",function(e){ keyUpHandler(e, "iz")} , false);
 der.addEventListener("touchend",function(e){ keyUpHandler(e, "der")} , false);
 
-// disparar.addEventListener('mousedown', function(event) {
-//     misiles.push({x: launcher.x + launcher.w*.5, y: launcher.y, w: 3, h: 10});
-// });
+fire.addEventListener("touchstart",function(e){ keyDownHandler(e, "fire")}, false);
 
-
+// recojo las pulsaciones y segun el boton dispara o se mueve
 function keyDownHandler(e, but) {
     if(e.keyCode == 39 || e.keyCode == 68) {
-        rightPressed = true;
+        derechaP = true;
     }
     else if(e.keyCode == 37 || e.keyCode == 65) {
-        leftPressed = true;
+        izquierdaP = true;
     } 
     else if(but == "der"){
-        rightPressed = true;
+        derechaP = true;
     } 
     else if(but == "iz"){
-        leftPressed = true;
+        izquierdaP = true;
     } else if(e.keyCode == 70 || e.keyCode == 32){
+        // evita que al pulsar el espacio al disparar vuelva al principio de la pagina
+        e.preventDefault()
+        // añade un misil al array de misiles
+        misiles.push({x: naveX + naveAncho*.5, y: naveY, w: 3,h: 10});
+    } else if(but == "fire"){
         e.preventDefault()
 
         misiles.push({x: naveX + naveAncho*.5, y: naveY, w: 3,h: 10});
     }
 
 }
+// detecta que se deja de pulsar la tecla o el boton
 function keyUpHandler(e, but) {
     if(e.keyCode == 39 || e.keyCode == 68) {
-        rightPressed = false;
+        derechaP = false;
     }
     else if(e.keyCode == 37 || e.keyCode == 65) {
-        leftPressed = false;
+        izquierdaP = false;
     }
     else if(but == "der"){
-        rightPressed = false;
+        derechaP = false;
     } 
     else if(but == "iz"){
-        leftPressed = false;
+        izquierdaP = false;
     }
 }
 
-
+// plantilla para la creacion de los enemigos
 var enemigo = function(options){
     return {
         id: options.id || '',
@@ -132,61 +131,45 @@ var enemigo = function(options){
         image: options.image || ''
     }
 }
-
-var enemies = [
-            //    new enemigo({id: "enemy1", x: ancho * 0.15, y: -(alto*0.03), w: ancho * 0.07, h: alto * 0.05 }),
-            //    new enemigo({id: "enemy2", x: 225, y: -20, w: ancho * 0.07, h: alto * 0.05 }),
-            //    new enemigo({id: "enemy3", x: 350, y: -20, w: 80, h: 30 }),
-            //    new enemigo({id: "enemy4", x:100,  y:-70,  w:80,  h: 30}),
-            //    new enemigo({id: "enemy5", x:225,  y:-70,  w:50,  h: 30}),
-            //    new enemigo({id: "enemy6", x:350,  y:-70,  w:50,  h: 30}),
-            //    new enemigo({id: "enemy7", x:475,  y:-70,  w:50,  h: 30}),
-            //    new enemigo({id: "enemy8", x:600,  y:-70,  w:80,  h: 30}),
-            //    new enemigo({id: "enemy9", x:475,  y:-20,  w:50,  h: 30}),
-            //    new enemigo({id: "enemy10",x: 600, y: -20, w: 50, h: 30}),
-
-            //    // Segundo grupo de enemigos
-            //    new enemigo({ id: "enemy11", x: 100, y: -220, w: 50, h: 30, image: enemiespic2 }),
-            //    new enemigo({ id: "enemy12", x: 225, y: -220, w: 50, h: 30, image: enemiespic2 }),
-            //    new enemigo({ id: "enemy13", x: 350, y: -220, w: 80, h: 50, image: enemiespic2 }),
-            //    new enemigo({ id: "enemy14", x: 100, y: -270, w: 80, h: 50, image: enemiespic2 }),
-            //    new enemigo({ id: "enemy15", x: 225, y: -270, w: 50, h: 30, image: enemiespic2 }),
-            //    new enemigo({ id: "enemy16", x: 350, y: -270, w: 50, h: 30, image: enemiespic2 }),
-            //    new enemigo({ id: "enemy17", x: 475, y: -270, w: 50, h: 30, image: enemiespic2 }),
-            //    new enemigo({ id: "enemy18", x: 600, y: -270, w: 80, h: 50, image: enemiespic2 }),
-            //    new enemigo({ id: "enemy19", x: 475, y: -200, w: 50, h: 30, image: enemiespic2 }),
-            //    new enemigo({ id: "enemy20", x: 600, y: -200, w: 50, h: 30, image: enemiespic2 })
-              ];
-
+// crea el array de enemigos
+var enemigos = [];
+// la posicion de los enemigos segun el nivel
+var enevel = (ancho / 500) * nivel
+// posicion X e Y de los enemigos
 var xene
 var yene = alto * 0.03
-for (let z = 0; z < CanteneNiv; z++) {    
+// bucle en el que se rellena el array de enemigos con los objetos de los enemgios del nivel
+for (let z = 0; z < CantEneNiv; z++) {   
+    // reinicia la posicion X a la primera columna del primero de cada fila 
     xene = ancho * 0.15
     for (let j = 0; j < 5; j++) {
+        // define que las lineas pares tengan una imagen distinta
         if(z%2 == 0){
-            enemies.push(new enemigo({id: "enemigo" + j + z, x: xene, y: -yene, w: ancho * 0.07, h: alto * 0.05, image: enemiespic2 }))
+            // mete el enemigo en el array asignandole la id con la fila y la columna, la posicion X e Y, el ancho y el alto y la imagen
+            enemigos.push(new enemigo({id: "enemigo" + j + z, x: xene, y: -yene, w: ancho * 0.07, h: alto * 0.05, image: eneFoto2 }))
         } else {
-            enemies.push(new enemigo({id: "enemigo" + j + z, x: xene, y: -yene, w: ancho * 0.07, h: alto * 0.05, image: enemiespic1 }))
+            enemigos.push(new enemigo({id: "enemigo" + j + z, x: xene, y: -yene, w: ancho * 0.07, h: alto * 0.05, image: eneFoto1 }))
         }
-        
+        // aumenta la posicion X a la que estara el siguiente enemigo
         xene += ancho * 0.17
         
     }
+    // aumenta la posicion para la siguiente linea
     yene += alto * 0.1
-}
+}             
 
-// ajustar tamaño de los muñecos + grande
-             
-
-function drawEnemies () {
-    if(enemies.length == 0){
-        gameFin = true
+function dibujaEnemigos () {
+    // si no quedan enemigos ganas y si no los dibuja
+    if(enemigos.length == 0){
+        // define juegoFin como true que indica que has ganado
+        juegoFin = true
     } else{
-        for (var i = 0; i < enemies.length; i++) {
-            // console.log(enemies[i]);
-            ctx.drawImage(enemies[i].image, enemies[i].x, enemies[i].y += nivel, enemies[i].w, enemies[i].h);
-            // Detects when ships hit lower level
-            colisionConNave(enemies[i]);
+        // recorre el array de enemigos y los dibuja
+        for (var i = 0; i < enemigos.length; i++) {
+            // dibuja al enemigo recogiendo la imagen, la posicion Y que aumenta segun el nivel, el ancho y el alto
+            ctx.drawImage(enemigos[i].image, enemigos[i].x, enemigos[i].y += enevel, enemigos[i].w, enemigos[i].h);
+            // llama al metodo que detecta que el enemigo llega al final
+            colisionConNave(enemigos[i]);
         }
     }
 }
@@ -194,112 +177,133 @@ function drawEnemies () {
 function colisionConNave (enemigo){
     // pierde llega abajo
     if(enemigo.y > alto){
-        gameFin = false
+        // define juegoFin como false que indica que has perdido
+        juegoFin = false
     }
 
-    // pierde choca
-   if ((enemigo.y < naveY + 25 && enemigo.y > naveY - 25) &&
-        (enemigo.x < naveX + 45 && enemigo.x > naveX - 45)) { // Checking if enemigo is on the left or right of spaceship
-            gameFin = false;
+    // pierde porque choca con la nave comprobando si el enemigo esta en contacto con la nave
+   if ((enemigo.y < naveY + (ancho * 0.04) && enemigo.y > naveY - (ancho * 0.04)) &&
+        (enemigo.x < naveX + (ancho * 0.075) && enemigo.x > naveX - (ancho * 0.075))) {
+            juegoFin = false;
         }
 
     
 }
 
 
-function drawNave() {
+function dibujaNave() {
     ctx.drawImage(naveImage, naveX ,naveY , naveAncho, naveAlto);
 }
 
-function drawScore() {
-    ctx.font = "16px Press Start 2P";
-    ctx.fillStyle = "#0095DD";
-    ctx.fillText("Score: "+ (scoreTotal + score), 8, 20);
-}
-
-function hitDetect (m, mi) {
-    for (var i = 0; i < enemies.length; i++) {
-        var e = enemies[i];
-        if(m.x+m.w >= e.x && 
-           m.x <= e.x+e.w && 
-           m.y >= e.y && 
-           m.y <= e.y+e.h){
-           misiles.splice(misiles[mi],1); 
-            enemies.splice(i, 1); 
-            score += 1
+function contactoMisil (m, mi) {
+    // bucle que comprueba en todos los enemigos si han entrado en contacto con un misil
+    for (var i = 0; i < enemigos.length; i++) {
+        //variable e se corresponde con un enemigo
+        var e = enemigos[i];
+        // comprueba si algun enemigo toca el misil
+        if(m.x+m.w >= e.x && m.x <= e.x+e.w && m.y >= e.y && m.y <= e.y+e.h){
+            // borra el misil del array
+            misiles.splice(misiles[mi],1); 
+            // borra el enemigo del array
+            enemigos.splice(i, 1); 
+            // suma un punto
+            puntuacion += 1
             
         }
     }
 }
 
-// est
-function drawMisiles(){
+
+function dibujaMisiles(){
+    // recorre el array de los misiles
     for(var i=0; i < misiles.length; i++){
+        // la variable m corresponde al misil
         var m = misiles[i];
         ctx.fillStyle = "green"
-        ctx.fillRect(m.x, m.y-=alto/60, m.w, m.h); // bullet direction
-        hitDetect(misiles[i],i);
-        if(m.y <= 0){ // If a missile goes past the canvas boundaries, remove it
-            misiles.splice(i,1); // splice that missile out of the misiles array
+        // dibuja el misil y le resta la velocidad
+        ctx.fillRect(m.x, m.y-=alto/60, m.w, m.h); 
+        // llama al metodo que comprueba si entra en contacto con un enemigo
+        contactoMisil(misiles[i],i);
+        // si el misil llega arriba lo quita del array
+        if(m.y <= 0){ 
+            misiles.splice(i,1); 
         }
     }
 }
 
 
 
-function draw() {
+function dibuja() {
+    // borra todo lo del canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height)
+    // dibuja el fondo
     ctx.drawImage(backgroundImage, 0, 0, ancho, alto)
-    drawNave();
-    drawScore();
-    drawMisiles();
-    drawEnemies();
-
-    if(gameFin === false){  
+    dibujaNave();
+    dibujaMisiles();
+    dibujaEnemigos();
+// si has perdido
+    if(juegoFin === false){  
+        // detiene el intervalo de dibujo
         clearInterval(id); 
+        // borra el canvas
         ctx.clearRect(0, 0, canvas.width, canvas.height);
+        // pone la imagen de fin has perdido
         ctx.drawImage(gameOver, 0, 0, canvas.width, canvas.height);
-        console.log("fin")
-        inicio = 0
-        EnvPunt()
-        boton.innerHTML= "restart";
+        
+        boton.innerHTML= "Restart";
+        // muestra las instrucciones
+        document.getElementById("instrucciones").style.display = "block";
+        document.getElementById("punt").innerHTML = "Tu puntuacion es de: " + (puntuacionTotal + puntuacion)
+        // setea el nivel a 1 y las filas de enemigos a 3
         nivel = 1;
-        CanteneNiv = 3
+        CantEneNiv = 3
+        // si estas logado envia la puntuacion y si no te lo indica
         if(usuario != null && usuario != ""){
             EnvPunt();
             console.log("envia puntos")
+        } else {
+            document.getElementById("punt").innerHTML = document.getElementById("punt").innerHTML + "<br>Para guardar su puntuacion debe de estar logado"
         }
+        // resetea el juego y la puntuacion
+        inicio = 0
+        puntuacionTotal = 0
         
-    } else if(gameFin === true){
+    } else if(juegoFin === true){
+        // si has ganado 
         clearInterval(id)
+        // borra el canvas y dibuja la imagen de ganar
         ctx.clearRect(0, 0, canvas.width, canvas.height)
         ctx.drawImage(youWin, 0, 0, canvas.width, canvas.height)
+        // muestra la puntucion
+        document.getElementById("instrucciones").style.display = "block";
+        document.getElementById("text").style.display = "none";
         console.log("gana")
         inicio = 0  
-        scoreTotal += score
+        // suma la puntuacion al total y suma el nivel y la linea de enemigos
+        puntuacionTotal += puntuacion
         nivel += 0.2;
-        CanteneNiv += 1
+        CantEneNiv += 1
     }
 
-    
-    if(rightPressed && naveX < canvas.width-naveAncho) {
-        naveX += 7;
+    // aumenta la posicion de la nave si esta pulsado el boton a la derecha o disminuye si esta pulsando la izquierda
+    if(derechaP && naveX < canvas.width-naveAncho) {
+        naveX += ancho/120;
     }
-    else if(leftPressed && naveX > 0) {
-        naveX -= 7;
+    else if(izquierdaP && naveX > 0) {
+        naveX -= ancho/120;
     }
 
 }
 
 
+// crea el intervalo
+id = setInterval(dibuja, 20);
 
-id = setInterval(draw, 20);
 
-
-
+// envia la puntuacion al php que actualiza la base de datos igual que en la clase de juego3.js de arkanoid
 function EnvPunt(){
 
-    console.log("Puntos en envio" + (scoreTotal + score))
+    console.log("Puntos en envio" + (puntuacionTotal + puntuacion))
 
     if (window.XMLHttpRequest) {
         peticion=new XMLHttpRequest(); 
@@ -308,7 +312,7 @@ function EnvPunt(){
           } 
 
           datos={}
-          datos.punt = (scoreTotal + score);
+          datos.punt = (puntuacionTotal + puntuacion);
           datos.user = usuario;
    
           console.log(datos.punt)
@@ -316,8 +320,6 @@ function EnvPunt(){
           peticion.onreadystatechange = function(){
         if(peticion.readyState == 4) {
           if (peticion.status == 200) {
-            //  var asd=JSON.parse(peticion.responseText)
-            //  console.log(asd)
             console.log(peticion.responseText)
             console.log("puntuacion enviada")
           }
@@ -334,15 +336,15 @@ function EnvPunt(){
 
 
 
-
+// inicia el juego al pulsar el boton
 boton.addEventListener('click', function(){
 if (inicio == 0){
-    gameFin = null
-    arkanoid();
+    juegoFin = null
+    naves();
     console.log("nivel boton: " + nivel)
 }
     this.innerHTML = "siguiente";
-    this.setAttribute("display", "none")
+    document.getElementById("instrucciones").style.display = "none";
 })
 
     
