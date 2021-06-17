e9c225348b73606516d50c42a3d05bfcb8b64ae5
hernandezFernandez <81775411+hernandezFernandez@users.noreply.github.com>
2021-06-17 09:45:07 +0200

    Add files via upload

--- juegos/arkanoid/Puntuacion.php
+++ juegos/arkanoid/Puntuacion.php
@@ -1,9 +1,9 @@
 <?php
-
+// archivo que incluye las conexiones
 include '../../conexion.php';
-
+// decodifica la puntuacion y el usuario que estaban en json
 $datos = json_decode(stripslashes($_POST['datos']));
-// no funciona la consulta por algun motivo
+// recoge los puntos del usuario que esta en el objeto $datos
 $MyBBDD->consulta("SELECT * FROM usuarios where id_usu = '" . $datos->user . "'");
     while ($fila = $MyBBDD->extraer_registro()) {
 
@@ -12,16 +12,16 @@ $MyBBDD->consulta("SELECT * FROM usuarios where id_usu = '" . $datos->user . "'"
       
     }
 
-
+// suma los puntos que tenia el usuario con los nuevos
     $punt_actu = $punt_actu + $datos->punt;
-
+// actualiza la puntuacion con la nueva
 $MyBBDD->consulta("UPDATE usuarios SET TotalPunt= '$punt_actu'  WHERE id_usu = '" . $datos->user . "'");
-
+// comprueba si la puntuacion maxima actual es mayor que la nueva no la actualiza
 if( $puntosMax < $datos->punt){
   $MyBBDD->consulta("UPDATE usuarios SET puntosMax= '" . $datos->punt  . "'  WHERE id_usu = '" . $datos->user . "'");
 
 }
-
+// devuelve la puntuacion para comprabarla por consola
 echo $datos->punt;
 
 ?>
--- juegos/arkanoid/juego3.js
+++ juegos/arkanoid/juego3.js
@@ -1,67 +1,73 @@
 
-var boton = document.getElementById("play");
+
+// variable que indica si el juego esta iniciado (0=no,1=si)
 var inicio = 0;
+// variables de botones
+var boton = document.getElementById("play");
 
 var iz = document.getElementById("iz");
 var der = document.getElementById("der");
 
 
-var scoreTotal = 0;
+var puntuacionTotal = 0;
 var nivel = 1;
 
 function arkanoid(){
+    // se inicia el juego
     inicio = 1;
+    // creacion de variables
+var backgroundImage = new Image();
+backgroundImage.src = "/tfg/img/fondoArk.jpg";
     
-
+var bola = new Image();
+bola.src = "/tfg/img/bola.png";
 
 var canvas = document.getElementById("myCanvas");
+// cambia el display del canvas de none a block
 canvas.style.display="block";
-
+// setea el ancho y el alto del canvas
 canvas.setAttribute('width', main.clientWidth * 0.8); 
 canvas.setAttribute('height', (main.clientWidth * 0.8) /1.5); 
-
+// guarda el alto y el ancho en una variable
 var ancho = main.clientWidth * 0.8;
 var alto = ((main.clientWidth * 0.8) /1.5);
 
 var ctx = canvas.getContext("2d");
 
 // tamaño del balon
-var ballRadius = ancho/48;
+var PelotaRadius = ancho/48;
 // posicion del balon
 var x = canvas.width/2;
 var y = canvas.height * 0.9;
-// velocidad de movimiento dle balon
+// velocidad de movimiento del balon
 var dx = (ancho/240)*nivel;
 var dy = -(ancho/240)*nivel;
 
-console.log(nivel)
 
 // alto y ancho de la pala
-var paddleHeight = alto/24;
-var paddleWidth = ancho/4;
+var PalaHeight = alto/24;
+var PalaWidth = ancho/4;
 // posicion de la pala
-var paddleX = (canvas.width-paddleWidth)/2;
+var PalaX = (canvas.width-PalaWidth)/2;
 // comprobar que boton esta pulsado
-var rightPressed = false;
-var leftPressed = false;
+var derechaP = false;
+var izquierdaP = false;
 // cantidad de ladrillos
 var brickRowCount = 5;
 var brickColumnCount = 4;
 
 // medidas de los ladrillos
-var brickWidth = ancho/6.4;
-var brickHeight = alto/16;
+var anchoLadrillo = ancho/6.4;
+var altoLadrillo = alto/16;
 // separacion entre ladrillos
-var brickPadding = ancho/48;
+var separacionLadrillo = ancho/48;
 // separacion de los ladrillos con los margenes
-var brickOffsetTop = ancho/10.6;
-var brickOffsetLeft = ancho/16;
+var separacionAlto = ancho/10.6;
+var separacionIz = ancho/16;
 // puntuacion
-var score = 0;
+var puntuacion = 0;
 // intervalo
 var id;
-// ciclos que lleva de ejecucion el juego
-var temp = 0;
 // imagenes
 var gameOver = new Image();
 gameOver.src = "../../img/gameOver.png";
@@ -72,199 +78,210 @@ youWin.src = "../../img/youWin.jpg";
 var pala = new Image();
 pala.src = "../../img/arkanoid.png"
 
-var iz = document.getElementById("iz");
-var der = document.getElementById("der");
 
-
-// arrai de ladrillos y en el que se generan los ladrillos, el staus indica si esta roto o sigue vivo
-var bricks = [];
+// array de ladrillos y en el que se generan los ladrillos, el status indica si esta roto o sigue vivo
+var Ladrillo = [];
 for(c=0; c<brickColumnCount; c++) {
-    bricks[c] = [];
+    Ladrillo[c] = [];
     for(r=0; r<brickRowCount; r++) {
-        bricks[c][r] = { x: 0, y: 0, status: 1 };
+        Ladrillo[c][r] = { x: 0, y: 0, status: 1 };
     }
 }
-
+// crea un evento de escucha para cuando se pulsa una tecla
 document.addEventListener("keydown", keyDownHandler, false);
 document.addEventListener("keyup", keyUpHandler, false);
 
-// document.addEventListener("mousemove", mouseMoveHandler, false);
-
+// crea los eventos de escucha para los botones de la version movil
 iz.addEventListener("touchstart",function(e){ keyDownHandler(e, "iz")} , false);
 der.addEventListener("touchstart",function(e){ keyDownHandler(e, "der")}, false);
 iz.addEventListener("touchend",function(e){ keyUpHandler(e, "iz")} , false);
 der.addEventListener("touchend",function(e){ keyUpHandler(e, "der")} , false);
-
+// comprueba el evento de pulsacion tanto de boton como de tecla y comprueba cual se ha pulsado
 function keyDownHandler(e, but) {
-    if(e.keyCode == 39) {
-        rightPressed = true;
+    if(e.keyCode == 39 || e.keyCode == 68) {
+        derechaP = true;
     }
-    else if(e.keyCode == 37) {
-        leftPressed = true;
+    else if(e.keyCode == 37 || e.keyCode == 65) {
+        izquierdaP = true;
     } 
     else if(but == "der"){
-        rightPressed = true;
+        derechaP = true;
     } 
     else if(but == "iz"){
-        leftPressed = true;
+        izquierdaP = true;
     }
 }
+// compruebo el evento de dejar de levantar la tecla o el boton
 function keyUpHandler(e, but) {
-    if(e.keyCode == 39) {
-        rightPressed = false;
+    if(e.keyCode == 39 || e.keyCode == 68) {
+        derechaP = false;
     }
-    else if(e.keyCode == 37) {
-        leftPressed = false;
+    else if(e.keyCode == 37 || e.keyCode == 65) {
+        izquierdaP = false;
     }
     else if(but == "der"){
-        rightPressed = false;
+        derechaP = false;
     } 
     else if(but == "iz"){
-        leftPressed = false;
+        izquierdaP = false;
     }
 }
-
-function collisionDetection() {
+// se comprueba en cada frame si la pelota esta tocando algun ladrillo
+function colision() {
     for(c=0; c<brickColumnCount; c++) {
         for(r=0; r<brickRowCount; r++) {
-            var b = bricks[c][r];
+            var b = Ladrillo[c][r];
+            // comprueba si el ladrillo esta activo
             if(b.status == 1) {
-                if(x + ballRadius >= b.x && x <= b.x+brickWidth && y + ballRadius >= b.y && y <= b.y+brickHeight) {
+                // si la posicion de la pelota mas su radio esta en contacto con la posicion del ladrillo mas su ancho y alto
+                if(x + PelotaRadius >= b.x && x <= b.x+anchoLadrillo && y + PelotaRadius >= b.y && y <= b.y+altoLadrillo) {
+                    // si entra en contacto la pelota cambia de direccion
                     dy = -dy;
+                    // cambia el estado del ladrillo a no activo
                     b.status = 0;
-                    score++;
-                    // gana
-                    if(score == brickRowCount*brickColumnCount) {
+                    // suma un punto
+                    puntuacion++;
+                    // si los puntos igualan al numero de ladrillos se gana
+                    if(puntuacion == brickRowCount*brickColumnCount) {
+                        // borra el canvas
                         ctx.clearRect(0, 0, canvas.width, canvas.height);
+                        // muestra la imagen de victoria
                         ctx.drawImage(youWin, 0, 0, canvas.width, canvas.height);
+                        // reinicia el juego
                         clearInterval(id);
                         inicio = 0;
+                        // muestra la puntuación
+                        document.getElementById("instrucciones").style.display = "block";
+                        document.getElementById("punt").innerHTML = "Tu puntuacion es de: " + (puntuacionTotal + puntuacion)
+                        document.getElementById("text").style.display = "none";
+                        // sube de nivel
                         nivel += 0.2;
-                        scoreTotal += score
-                        console.log("suma nivel: " + nivel)
+                        // suma la puntuacion de esta fase a la total de la partida
+                        puntuacionTotal += puntuacion
                     }
                 }
             }
         }
     }
 }
-
-function drawBall() {
-    ctx.beginPath();
-    ctx.arc(x, y, ballRadius, 0, Math.PI*2);
-    ctx.fillStyle = "#0095DD";
-    ctx.fill();
-    ctx.closePath();
+// dibuja la pelota
+function dibujaPelota() {
+    ctx.drawImage(bola, x, y, ancho/15, ancho/15)
 }
-function drawPaddle() {
-   
-    ctx.drawImage(pala, paddleX, canvas.height-paddleHeight, paddleWidth, paddleHeight);
+// dibuja la pala
+function dibujaPala() {
+//    dibuja la pala, se especifican la imagen, la posicion X e Y y sus dimensiones
+    ctx.drawImage(pala, PalaX, canvas.height-PalaHeight, PalaWidth, PalaHeight);
 }
-function drawBricks() {
+// dibuja los ladrillos
+function dibujaLadrillo() {
     for(c=0; c<brickColumnCount; c++) {
         for(r=0; r<brickRowCount; r++) {
-            if(bricks[c][r].status == 1) {
-                var brickX = (r*(brickWidth+brickPadding))+brickOffsetLeft;
-                var brickY = (c*(brickHeight+brickPadding))+brickOffsetTop;
-                bricks[c][r].x = brickX;
-                bricks[c][r].y = brickY;
+            // recorre el array de los ladrillo y dibuja los que estan activos
+            if(Ladrillo[c][r].status == 1) {
+                // para la posicion del ladrillo se multiplica la fila por la suma del ancho del ladrillo y la separacion entre ladrillos y se le suma la separacion izquierda
+                var ladrilloX = (r*(anchoLadrillo+separacionLadrillo))+separacionIz;
+                var ladrilloY = (c*(altoLadrillo+separacionLadrillo))+separacionAlto;
+                // setea la posicion en el array
+                Ladrillo[c][r].x = ladrilloX;
+                Ladrillo[c][r].y = ladrilloY;
                 ctx.beginPath();
-                ctx.rect(brickX, brickY, brickWidth, brickHeight);
-                ctx.fillStyle = "#0095DD";
+                // dibuja el ladrillo
+                ctx.rect(ladrilloX, ladrilloY, anchoLadrillo, altoLadrillo);
+                ctx.fillStyle = " #ff8000";
                 ctx.fill();
                 ctx.closePath();
             }
         }
     }
 }
-function drawScore() {
-    ctx.font = "16px Press Start 2P";
-    ctx.fillStyle = "#0095DD";
-    ctx.fillText("Score: "+ (scoreTotal + score), 8, 20);
-}
-
-function draw() {
-    temp += 1
+// dibuja el juego
+function dibuja() {
+    // borra el canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    drawBricks();
-    drawBall();
-    drawPaddle();
-    drawScore();
-    collisionDetection();
+    // dibuja el fondo
+    ctx.drawImage(backgroundImage, 0, 0, ancho, alto)
+
+    dibujaLadrillo();
+    dibujaPelota();
+    dibujaPala();
+    colision();
 // toca pared
-    if(x + dx > canvas.width-ballRadius || x + dx < ballRadius) {
+    if(x + dx > canvas.width-PelotaRadius || x + dx < PelotaRadius) {
         dx = -dx;
     }
 // Toca techo
-    if(y + dy < ballRadius) {
+    if(y + dy < PelotaRadius) {
         dy = -dy;
     }
 // toca la pala o se cae   
-    if(y + dy >= canvas.height - paddleHeight - ballRadius) {
-        if(x + ballRadius > paddleX  && x < paddleX + paddleWidth) {
+    if(y + dy >= canvas.height - PalaHeight - PelotaRadius) {
+        if(x + PelotaRadius > PalaX  && x < PalaX + PalaWidth) {
             dy = -dy;
-        } 
-        else{
-            // fin del juego
+        }else{
+            // fin del juego pierdes
             ctx.clearRect(0, 0, canvas.width, canvas.height);
+            // muestra imagen perder
             ctx.drawImage(gameOver, 0, 0, canvas.width, canvas.height);
+            // reinicia el intervalo
             clearInterval(id);
             inicio = 0;
-            console.log(usuario)
-            console.log("fin: " + (scoreTotal + score));
-            boton.innerHTML= "restart";
+            // cambia el boton a restart
+            boton.innerHTML= "Restart";
+            // muestra la puntuacion
+            document.getElementById("instrucciones").style.display = "block";
+            document.getElementById("text").style.display = "none";
+            document.getElementById("punt").innerHTML = "Tu puntuacion es de: " + (puntuacionTotal + puntuacion)
+            // resetea el nivel a 1
             nivel = 1;
+            // si esta logueado se envia la puntuacion y en caso de no estarlo indica que la puntuacion no se guardara
             if(usuario != null && usuario != ""){
                 EnvPunt();
-                console.log("envia puntos")
+            } else {
+                document.getElementById("punt").innerHTML = document.getElementById("punt").innerHTML + "<br>Para guardar su puntuacion debe de estar logado"
             }
-
-
         }
     }
-    
-    if(rightPressed && paddleX < canvas.width-paddleWidth) {
-        paddleX += canvas.width/68;
+    // si derechaP es true y no esta al borde derecho aumenta la posicion x de la pala
+    if(derechaP && PalaX < canvas.width-PalaWidth) {
+        PalaX += canvas.width/68;
+    }else if(izquierdaP && PalaX > 0) {
+     // si izquierdaP es true y no esta al borde izquierdo disminuye la posicion x de la pala
+        PalaX -= canvas.width/68;
     }
-    else if(leftPressed && paddleX > 0) {
-        paddleX -= canvas.width/68;
-    }
-    
+    // aumenta la posicion de la bola(x e Y) con la velocidad de la bola(dx y dy)
     x += dx;
     y += dy;
 }
 
 
-
-id = setInterval(draw, 10);
-
+// crea el intervalo del dibujado
+id = setInterval(dibuja, 10);
+// funcion para enviar los puntos
 function EnvPunt(){
 
-    console.log("Puntos en envio" + (scoreTotal + score))
 
     if (window.XMLHttpRequest) {
         peticion=new XMLHttpRequest(); 
           } else if (window.ActiveXObject) {
         peticion=ActiveXObject("Microsoft.XMLHTTP"); 
           } 
-
+// crea un objeto con la puntuacion y el usuario
           datos={}
-          datos.punt = (scoreTotal + score);
+          datos.punt = (puntuacionTotal + puntuacion);
           datos.user = usuario;
    
-          console.log(datos.punt)
 
           peticion.onreadystatechange = function(){
         if(peticion.readyState == 4) {
           if (peticion.status == 200) {
-            //  var asd=JSON.parse(peticion.responseText)
-            //  console.log(asd)
+            
             console.log(peticion.responseText)
             console.log("puntuacion enviada")
           }
         }
           }
-   
+//    envia la peticion con POST y los datos en JSON
           peticion.open("POST", "./Puntuacion.php");
           peticion.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
           peticion.send("datos="+JSON.stringify(datos));
@@ -275,14 +292,15 @@ function EnvPunt(){
 
 
 
-
+// inicia el juego al pulsar el boton de inicio
 boton.addEventListener('click', function(){
 if (inicio == 0){
     arkanoid();
     console.log("nivel boton: " + nivel)
 }
     this.innerHTML = "siguiente";
-    this.setAttribute("display", "none")
+    // oculta las instrucciones al iniciar el juego
+    document.getElementById("instrucciones").style.display = "none";
 })
 
     
